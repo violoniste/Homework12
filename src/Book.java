97fe212510800065462fefa71ef8778caa4fc874
BlackIron <violoniste2@gmail.com>
2023-08-18 22:51:30 +0700

    Готово

---
+++ src/Book.java
@@ -0,0 +1,27 @@
+public class Book {
+    private final String name;
+    private final Author author;
+    private int year;
+
+    public Book(String name, Author author, int year) {
+        this.name = name;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
